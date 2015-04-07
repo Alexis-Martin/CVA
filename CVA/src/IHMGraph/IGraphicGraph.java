f0435edbf09b20a305df341a0e8ce06a3870f848
tgeral68 <thomas.l.p.gerald@gmail.com>
2015-04-07 20:37:06 +0200

    IGRAPHIC GRAPH MAJ

--- CVA/src/IHMGraph/IGraphicGraph.java
+++ CVA/src/IHMGraph/IGraphicGraph.java
@@ -2,7 +2,14 @@ package IHMGraph;
 
 import java.awt.Component;
 
+import CVAGraph.AGraph;
+
 public interface IGraphicGraph {
 	
 	public Component getGraphicGraphComponent();
+	public AGraph getAGraph();
+	public void refresh();
+	public void replace();
+	
+	public void setAutomaticTopology(boolean automatic);
 }
